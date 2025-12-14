e6ede5d4f5e1ec30b7a95ec13ea0a41cc6e8d065
Cartwright Reed <cart@shipstone.com>
2025-12-13 20:58:43 -0500

    Bump SW cache to v12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ruzzle-v11';
+const CACHE_NAME = 'ruzzle-v12';
 const ASSETS = ['/', '/index.html', '/styles.css', '/game.js', '/manifest.json'];
 
 self.addEventListener('install', e => {
